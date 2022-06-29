f08c6f4f2a8089f5ea685482d581659b5ba2a6cb
Gustavo <gustavopolonio1@gmail.com>
2022-06-29 11:29:32 -0300

    first middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ app.use(express.json())
 
 const customers = []
 
+// Middleware
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found." })
+  }
+
+  req.customer = customer
+
+  return next()
+}
+
 app.post("/account", (req, res) => {
   const { name, cpf } = req.body
 
@@ -25,11 +39,12 @@ app.post("/account", (req, res) => {
   return res.status(201).send()
 })
 
-app.get("/statement/:cpf", (req, res) => {
-  const { cpf } = req.params
-  const customer = customers.find(customer => customer.cpf === cpf)
+// app.use(verifyIfExistsAccountCPF)
 
-  return res.status(200).json( customer.statement )
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req
+
+  return res.json( customer.statement )
 })
 
 
